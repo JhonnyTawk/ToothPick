72c1aa19913e79bbd32a97ca221462fd051fa67c
Jhonny <Jhonnytawk@mcloudagency.com>
2023-02-04 02:12:33 +0200

    Edit Cell

--- ToothPick/ToothPick/Posts/PostsCell/PostsCell.swift
+++ ToothPick/ToothPick/Posts/PostsCell/PostsCell.swift
@@ -13,8 +13,18 @@ class PostsCell: UITableViewCell {
     class var identifier: String { return String(describing: self) }
     class var nib: UINib { return UINib(nibName: identifier, bundle: nil) }
     
-    @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var descLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel! {
+        didSet {
+            titleLabel.numberOfLines = 0
+            titleLabel.font = UIFont.systemFont(ofSize: 16, weight: .bold)
+        }
+    }
+    @IBOutlet weak var descLabel: UILabel! {
+        didSet {
+            descLabel.numberOfLines = 0
+            descLabel.font = UIFont.systemFont(ofSize: 12, weight: .light)
+        }
+    }
     
     override func awakeFromNib() {
         super.awakeFromNib()
